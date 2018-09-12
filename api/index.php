db09e745a7df53dece283d9ba820c6c240d8f4f6
millea <millea1994@gmail.com>
2018-09-12 22:37:59 +0700

    add validation, get prev command

--- api/index.php
+++ api/index.php
@@ -4,13 +4,21 @@ $conn = mysqli_connect('localhost','root', '',  'data_bot');
 //get the json key
 $command  = $_POST['command'];
 $table  = $_POST['table'];
+$column  = $_POST['column'];
 
-getTable($table);
-
+call_user_func_array($command, array($table, $column));
 // create a function
-function getTable($table){
+function db($table){
   global $conn;
+
   $sql = "SELECT * FROM $table";
+  $result = mysqli_query($conn,$sql);
+  echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
+}
+//table
+function table($table, $column){
+  global $conn;
+  $sql = "SELECT * FROM $table where title = '$column'";
   $result = mysqli_query($conn,$sql);
   echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
 }
